6401dea1292640e7d45a218c7c2c6caba7486c77
Mofin52 <a.velikorechin@gmail.com>
2017-12-22 05:48:47 +0300

    added comments

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,6 @@
-//wait for user to put number
-    //if correct - make number from input, change ? sign to input
-    //else - make number in input red, in equation - background yellow
-
-//when all numbers set correct - change ? to input
-//wait for user to put number
-    //if correct - make number from input
-    //else - make number in input red
-
+/*
+ Saving most important elements to variables.
+ */
 var first = document.getElementById("first");
 var second = document.getElementById("second");
 var pict = document.getElementById("pict");
@@ -14,14 +8,27 @@ var scale = document.getElementById("scale");
 var result = document.getElementById("result");
 var equation = document.querySelector("#numSum p");
 
+/*
+ Answer of main equation - will initialize during app initialization
+ */
 var answer;
+
+/*
+ Count of round. Needed to know at which part of page we need to make changes
+ */
 var round = 0;
 
+/*
+ Initialization. Generates equation and shows the first arrow.
+ */
 function init(){
     generateNumbers();
     showArrowShape(first);
 }
 
+/*
+ Method to show arrows. Elem - is operand of equation to which arrow corresponds.
+ */
 function showArrowShape(elem){
     var unit = 38.7;
     var arrowSize = elem.textContent * unit;
@@ -34,6 +41,9 @@ function showArrowShape(elem){
     pict.insertBefore(inp, arrow);
 }
 
+/*
+ Method created inputs for arrows and final part of equation.
+ */
 function getInput(place, isForArrows) {
     var inp = document.createElement("input");
     inp.classList.add("numInput");
@@ -48,6 +58,12 @@ function getInput(place, isForArrows) {
     return inp;
 }
 
+/*
+ Listener that reacts on keyup event.
+ 1. Looks what round it is to know what values to compare.
+ 2. Compares value of input to corresponding part of equation.
+ 3. Depending on result of comparison either highlights mistakes or move us to the next round.
+ */
 function checkResult() {
     var guess = this.value;
     if (round == 0) {
@@ -82,13 +98,20 @@ function checkResult() {
         }
     }
 }
-
+/*
+ Method to init last iteration. Could be refactored and combined with showArrowShape but I have no time.
+ */
 function lastIter() {
     var inp = getInput(null, false);
     inp.classList.add("finalInput");
     equation.replaceChild(inp, result);
 }
-
+/*
+ Methods prepares text node that will replace input after right answer.
+ value - what to write in node
+ prevelem - element we want to replace (needed to copy JS-calculated styles).
+ isForArrows - if true, we copy calculated styles, if false - we skip it.
+ */
 function prepareReplaceNode(value, prevElem, isForArrows) {
     var newNode = document.createElement("span");
     newNode.classList.add("numInput");
@@ -104,6 +127,9 @@ function prepareReplaceNode(value, prevElem, isForArrows) {
     return newNode;
 }
 
+/*
+ Misc method to generate equation.
+ */
 function generateNumbers(){
     var firstValue = giveRandomNum(6, 9);
     var secondValue = giveRandomNum(11 - firstValue, 14 - firstValue);
@@ -112,8 +138,14 @@ function generateNumbers(){
     second.textContent = secondValue;
 }
 
+/*
+  Misc method to generate random numbers in given ranges.
+ */
 function giveRandomNum(lowLimit, highLimit){
     return Math.round(lowLimit - 0.5 + Math.random() * (highLimit - lowLimit + 1));
 }
 
+/*
+  Initialisation of app.
+ */
 window.onload = init;
